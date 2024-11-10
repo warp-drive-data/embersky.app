fc91a87f554a3fa6256cfa5daf77bbe21aa0a2a9
Chris Thoburn <runspired@users.noreply.github.com>
2024-11-10 00:35:40 -0800

    stub out the bsky api

---
+++ app/data/builders/app/bsky/video.ts
@@ -0,0 +1,5 @@
+export function getJobStatus() {}
+
+export function getUploadLimits() {}
+
+export function uploadVideo() {}
---
+++ app/data/builders/chat/bsky/moderation.ts
@@ -0,0 +1,5 @@
+export function getActorMetadata() {}
+
+export function getMessageContext() {}
+
+export function updateActorAccess() {}
